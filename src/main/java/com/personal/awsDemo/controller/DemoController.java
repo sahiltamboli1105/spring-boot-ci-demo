3a36a3860e7a1ef57305b19e6f0aed4f08a4c963
sahiltamboli1105 <iamthat1105@gmail.com>
2026-03-06 12:33:33 +0530

    update

---
+++ src/main/java/com/personal/awsDemo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.personal.awsDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/getResponse")
+    public String hello(){
+        return "Hello from CI/CD pipeline";
+    }
+}
